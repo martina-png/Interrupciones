b4a14f71dc08d116858e23cfb7f065bd4a292dd8
martina-png <84143039+martina-png@users.noreply.github.com>
2021-05-27 17:04:30 -0300

    Add files via upload

---
+++ Ejercicio3_27-05/Ejercicio3_27-05.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+Servo myservo;
+
+const int pin = 2;
+
+int pos = 1;
+int valor = 1;
+
+void setup(){
+ attachInterrupt(digitalPinToInterrupt(pin),boton,RISING);
+ myservo.attach(11);
+}
+
+void loop(){
+  pos = pos + valor;
+  if(pos > 180 || pos < 0){
+   valor = valor * -1; 
+  }
+  myservo.write(pos);
+  delay(20);
+}
+
+void boton(){
+  valor = valor * -1;
+}
